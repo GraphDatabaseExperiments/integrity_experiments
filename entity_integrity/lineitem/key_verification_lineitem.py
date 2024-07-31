5f866c48bb7210042405b72784faf4d18e83c4e7
GraphDatabaseExperiments <110078979+GraphDatabaseExperiments@users.noreply.github.com>
2024-07-31 14:46:49 +1200

    Update key_verification_lineitem.py

--- entity_integrity/lineitem/key_verification_lineitem.py
+++ entity_integrity/lineitem/key_verification_lineitem.py
@@ -123,8 +123,8 @@ def write_to_excel(filename, sheetname, experiment_name, heading: list, content:
 def main():
 
     #local bolt and http port, etc:
-    local_bolt = "bolt://localhost:7687"
-    local_pw = "Pskav752$api"
+    local_bolt = <local_bolt>
+    local_pw = <password>
     local_user = "neo4j"
 
     # Initialise DB
@@ -159,9 +159,9 @@ def main():
 
     for index in range(len(percentage_to_update)):
 
-        runs = 1
+        runs = 20
 
-        outliers = 0
+        outliers = 5
 
         for i in range(0, runs):
             
